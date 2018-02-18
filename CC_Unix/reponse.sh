a45393d57a7f80bef30918421ebedc26045c1f05
Myv-hs <matthewcoyle@me.com>
2018-02-18 18:11:07 +0100

    missing a two

--- CC_Unix/reponse.sh
+++ CC_Unix/reponse.sh
@@ -11,6 +11,6 @@ grep '[a-zA-Z][a-zA-Z]\s*[0-9]\{1,3\}\s*[a-zA-Z][a-zA-Z]' rep/immatriculations_v
 paste <(cut -d $'\t' -f2 rep/dir2/data) <(cut -d $'\t' -f1,3 rep/dir2/data) > rep/dir2/villes
 paste <(cat rep/dir2/data2 | tr -s $'\t' | cut -d $'\t' -f2) <(cat rep/dir2/data2 |tr -s $'\t'| cut -d $'\t' -f1,3) > rep/dir2/villes2
 diff rep/dir2/data rep/dir2/data2
-sort rep/dir2/data -t $'\t' -k
+sort rep/dir2/data -t $'\t' -k2
 sort rep/dir2/data -nt $'\t' -k3 | cut -d $'\n' -f1-3 > resultat
 alias bonjour='clear; echo Bonjour $USER, ce terminal est ouvert depuis $SECONDS secondes.'
